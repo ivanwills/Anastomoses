b3344901d4d11e5f1cba1f299a013e9d718060a1
Ivan Wills <ivan.wills@gmail.com>
2010-10-16 16:35:30 +1100

    Got question generation working

--- lib/Questions/Controller/Question.pm
+++ lib/Questions/Controller/Question.pm
@@ -2,8 +2,76 @@ package Questions::Controller::Question;
 use Moose;
 use namespace::autoclean;
 use Data::Dumper qw/Dumper/;
+use List::Util qw/shuffle/;
 
 BEGIN {extends 'Catalyst::Controller'; }
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $cat = $c->model('QuestionsDB')->resultset('Category')->find($c->request->params->{category_id});
+
+    $c->stash->{category}  = $cat->category;
+    $c->stash->{questions} = $self->create_questions($c, $c->request->params->{category_id} );
+    $c->stash->{question_count} = scalar @{ $c->stash->{questions} };
+
+    return;
+}
+
+sub create_questions {
+    my ( $self, $c, $category_id ) = @_;
+    my @questions;
+
+    return $c->session->{questions} if $c->session->{category_id} && $category_id == $c->session->{category_id};
+
+    my $qav       = $c->model('QuestionsDB')->resultset('QuestionAnswerView');
+    my $questions = $qav->search(
+        { category_id => $category_id },
+        { order_by => 'RANDOM()' },
+    );
+
+    while ( my $question = $questions->next ) {
+        push @questions, {
+            question    => $question->question,
+            question_id => $question->question_id,
+            answer_id   => $question->answer_id,
+            answers     => [{
+                answer    => $question->answer,
+                answer_id => $question->answer_id,
+            }],
+        };
+
+        my $other = $qav->search(
+            {
+                category_id => $category_id,
+                question_id => { '!=' => $question->question_id },
+                answer      => { '!=' => $question->answer },
+            },
+            {
+                order_by => 'RANDOM()',
+            },
+        );
+
+        for ( 0 .. 3 ) {
+            my $answer = $other->next;
+            push @{ $questions[-1]{answers} }, {
+                answer    => $answer->answer,
+                answer_id => $answer->answer_id,
+            };
+        }
+
+        @{ $questions[-1]{answers} } = shuffle @{ $questions[-1]{answers} };
+    }
+
+    $c->session->{category_id} = $category_id;
+    return $c->session->{questions} = \@questions;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__DATA__
 
 =head1 NAME
 
@@ -15,43 +83,7 @@ Catalyst Controller.
 
 =head1 METHODS
 
-=cut
-
-
 =head2 index
-
-=cut
-
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-    my @questions;
-
-    my $qas       = $c->model('QuestionsDB')->resultset('QuestionAnswer');
-    my $answers   = $c->model('QuestionsDB')->resultset('Answer');
-    my $questions = $c->model('QuestionsDB')->resultset('Question')->search(
-        { category_id => $c->request->params->{category_id} },
-#        { join => 'QuestionAnswer' }
-        { order_by => 'RANDOM()' },
-    );
-
-    while ( my $question = $questions->next ) {
-        my $qa = $qas->search({ question_id => $question->question_id })->first;
-        my $answer = $answers->search({ answer_id => $qa->answer_id })->first;
-        push @questions, {
-            question    => $question->question,
-            question_id => $question->question_id,
-            answer_id   => $answer->answer_id,
-            answers     => [{
-                answer    => $answer->answer,
-                answer_id => $answer->answer_id,
-            }],
-        };
-    }
-
-    $c->stash->{questions} = \@questions;
-
-    return;
-}
 
 =head1 AUTHOR
 
@@ -63,7 +95,3 @@ This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
-__PACKAGE__->meta->make_immutable;
-
-1;
